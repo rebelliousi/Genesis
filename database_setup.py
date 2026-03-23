fb31e7569894da20a0f192182e71e22865d8fdf1
rebellious_i <learneryolo3@gmail.com>
2026-03-23 18:40:05 +0500

    no change

--- database_setup.py
+++ database_setup.py
@@ -1,19 +1,10 @@
 import sqlite3
 from datetime import datetime
-from contextlib import contextmanager
 
-@contextmanager
-def get_db():
+def create_connection():
+    """Veritabanı bağlantısını oluşturur."""
     conn = sqlite3.connect('genesis.db')
-    conn.row_factory = sqlite3.Row # Bu çok önemli! Verileri (0,1,2) diye değil 'id', 'lat' diye çekmeni sağlar.
-    try:
-        yield conn
-        conn.commit() # Her şey yolundaysa otomatik kaydet
-    except Exception as e:
-        conn.rollback() # Hata varsa yapılanları geri al (Veri güvenliği)
-        raise e
-    finally:
-        conn.close() # Ne olursa olsun bağlantıyı kapat
+    return conn
 
 def setup_table():
     """
